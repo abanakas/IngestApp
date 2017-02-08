3d81598b19fa8a460d779841f5b84188fc913287
Alex Banakas <abanakas@gmail.com>
2017-02-08 16:20:07 -0500

    New

---
+++ IngestApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(IngestApp.Startup))]
+namespace IngestApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
